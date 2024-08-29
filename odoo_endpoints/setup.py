0d4eaec458c427f80e58ec729f7933d0c1de6add
MED-B <77291384+MED-B@users.noreply.github.com>
2024-08-29 07:40:04 +0100

    Add files via upload

---
+++ odoo_endpoints/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name="Odoo Endpoints",
+    version='0.0.1',
+    description='A package to unify odoo endpoints responses templates',
+    url='?',
+
+    author='MED-B',
+    author_email='MED-B',
+    license='BSD 2-clause',
+    #packages=[],
+    install_requires=['odoo'],
+
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
